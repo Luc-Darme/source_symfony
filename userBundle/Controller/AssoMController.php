a22fe3e7b869f872823e59577d35a645af40d91c
vianneylanguille <vianneylanguille@users.noreply.github.com>
2014-09-05 20:13:35 +0200

    Update AssoMController.php
    added pagination for news

--- userBundle/Controller/AssoMController.php
+++ userBundle/Controller/AssoMController.php
@@ -60,7 +60,7 @@ class AssoMController extends Controller
         $actionManager = $this->get('spy_timeline.action_manager');
         $subject = $actionManager->findOrCreateComponent($user);
         $timelineManager = $this->get('spy_timeline.timeline_manager');
-        $timeline = $timelineManager->getTimeline($subject);
+        $timeline = $timelineManager->getTimeline($subject, array('page' => 1, 'max_per_page' => '10', 'paginate' => true));
         
         return $this->render('ecloreuserBundle:AssoM:home.html.twig', array('timeline_coll'=>$timeline));
          
