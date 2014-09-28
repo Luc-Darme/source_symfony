43733e4524223718fd82d513ded65089dd258086
vianneylanguille <vianneylanguille@users.noreply.github.com>
2014-09-28 15:13:13 +0200

    Update TimelineEvents.php
    added pending_validation event

--- userBundle/Timeline/TimelineEvents.php
+++ userBundle/Timeline/TimelineEvents.php
@@ -22,5 +22,7 @@ final class TimelineEvents
     // instM:to instM of same inst, youngs of same inst OK
     // young:to young's inst instM and young from inst OK
   const onNewUser = 'ecloreuser.timeline.new_user';
+    // when something needs to be validated by admin, send an email
+  const pendingValidation = 'ecloreuser.timeline.pending_validation';
     
 }
